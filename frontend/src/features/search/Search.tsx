cf28aeabad0178b77feef11868dad36591be7e43
LukeScales1 <luke@nory.ai>
2023-08-20 14:11:56 -0700

    Disables search term input and search button on error or loading of articles

--- frontend/src/features/search/Search.tsx
+++ frontend/src/features/search/Search.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import {IconButton, Input, InputGroup, InputLeftElement} from '@chakra-ui/react'
 import { SearchIcon } from "@chakra-ui/icons";
+import { useGetArticlesQuery } from "../../redux/apiSlice";
 
 type Props = {
   updateSearchTerms: (searchTerms: string) => void;
@@ -10,20 +11,30 @@ type Props = {
 export const Search: React.FC<Props> = ({
   updateSearchTerms,
 }) => {
+  const { error, isLoading } = useGetArticlesQuery('');
+
   const [searchTerms, setSearchTerms] = React.useState<string>("");
   const handleSearchTermsChange = (event: React.ChangeEvent<HTMLInputElement>) => setSearchTerms(event.target.value);
   const handleClickSearch = () => { updateSearchTerms(searchTerms) };
 
+  const isSearchingDisabled = error ? true : isLoading;
+
   return (
     <InputGroup width={"60%"} marginBottom={"8px"}>
       <InputLeftElement>
-        <IconButton aria-label='Search articles' icon={<SearchIcon />} onClick={handleClickSearch}/>
+        <IconButton
+          aria-label='Search articles'
+          icon={<SearchIcon />}
+          onClick={handleClickSearch}
+          isDisabled={isSearchingDisabled}
+        />
       </InputLeftElement>
       <Input
         placeholder='Search your articles...'
         textAlign={"center"}
         value={searchTerms}
         onChange={handleSearchTermsChange}
+        isDisabled={isSearchingDisabled}
       />
     </InputGroup>
   )
