2e90a2539cb88f20656940a531865080db38e2bf
luke <luke@nory.ai>
2023-08-21 20:14:51 -0700

    upgrades fastAPI, implements app lifespan mgmt for indexing on startup, adds session mgmt for DB connections and requests

--- backend/src/main.py
+++ backend/src/main.py
@@ -2,17 +2,19 @@ from __future__ import annotations
 
 import logging
 import time
-from typing import Union
+from contextlib import asynccontextmanager
+from typing import Annotated, Optional, Union
 
 import requests
 import wikipedia.service as article_service
-from fastapi import FastAPI, HTTPException, Query
+from fastapi import Depends, FastAPI, Query
 from fastapi.middleware.cors import CORSMiddleware
 from index.indexer import create_or_update_inverted_index, rank_documents
 from index.nlp import lemmatize, set_up_nltk
 from index.schema import SearchResult
 from requests import Session
 from sqlalchemy import create_engine
+from sqlalchemy.orm import Session as DBSession
 from sqlalchemy.orm import sessionmaker
 from wikipedia.client import fetch_article_list, fetch_parsed_text
 from wikipedia.models import Article
@@ -23,35 +25,31 @@ logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 logging.basicConfig()
 
-app = FastAPI()
-
-origins = [
-    "http://localhost",
-    "http://localhost:3000",
-]
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
-
 set_up_nltk()
 
-s = requests.Session()
 text_processor = lemmatize
 
 NUMBER_OF_ARTICLES = 10
 
 
 DB_CONNECTION = "postgresql+psycopg2://postgres:password@db:5432/wiki-search"
-
 engine = create_engine(DB_CONNECTION)
+db_session_maker = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-DbSession = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-db_session = DbSession()
+
+async def get_db_session():
+    """
+    Get a managed database session from the database connection pool.
+    """
+    db_session = None
+    try:
+        db_session = db_session_maker()
+        yield db_session
+    finally:
+        if db_session:
+            db_session.close()
+
+DbSessionDeps = Annotated[Optional[DBSession], Depends(get_db_session)]
 
 
 def get_and_parse_random_articles(
@@ -71,7 +69,8 @@ def get_and_parse_random_articles(
     ]
 
 
-def _index_documents(session: Session):
+def _index_documents(db_session: DBSession):
+    """ Helper function for indexing documents. """
     logger.info("Indexing documents...")
     start_time = time.time()
     fetch_time = None
@@ -80,7 +79,8 @@ def _index_documents(session: Session):
 
     if not db_articles:
         logger.info("No articles found in database. Fetching some from Wikipedia...")
-        articles = get_and_parse_random_articles(session, ArticleTitlesGet(rnlimit=NUMBER_OF_ARTICLES))
+        with requests.Session() as session:
+            articles = get_and_parse_random_articles(session, ArticleTitlesGet(rnlimit=NUMBER_OF_ARTICLES))
         fetch_time = time.time()
         logger.info(f"{len(articles)} articles fetched!")
         article_service.add_articles_bulk(
@@ -108,7 +108,27 @@ def _index_documents(session: Session):
     logger.info(f"Time taken to index articles: {index_stop_time - index_start_time}")
 
 
-_index_documents(s)
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    db_session = db_session_maker()
+    _index_documents(db_session)
+    db_session.close()
+    yield
+
+
+app = FastAPI(lifespan=lifespan)
+origins = [
+    "http://localhost",
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/")
@@ -118,25 +138,27 @@ async def index():
 
 
 @app.get("/articles", response_model=list[ArticleSchema])
-async def get_articles(limit: int = Query(default=0, gt=0, le=100), offset: int = Query(default=0, ge=0)):
+async def get_articles(
+        db_session: DbSessionDeps,
+        limit: int = Query(default=0, gt=0, le=100),
+        offset: int = Query(default=0, ge=0),
+):
     """ Get articles from the DB, with optional limit and offset.
 
     Defaults to all articles in the DB if no limit provided. Offset can be used without a limit.
 
+    :param db_session: The database session.
     :param limit: The maximum number of articles to return.
     :param offset: The number of articles to skip.
-    :raises HTTPException: (404) If no articles are found.
     """
-    articles = article_service.filter_articles(session=db_session, limit=limit, offset=offset)
-    if not articles:
-        raise HTTPException(status_code=404, detail="No articles found!")
-    return articles
+    return article_service.filter_articles(session=db_session, limit=limit, offset=offset)
 
 
 @app.post("/articles", response_model=list[ArticleSchema])
-async def fetch_new_articles():
+async def fetch_new_articles(db_session: Annotated[DBSession, Depends(get_db_session)]):
     """ Get some random articles from Wikipedia, add to the DB, and reindex. """
-    new_articles = get_and_parse_random_articles(s, ArticleTitlesGet(rnlimit=NUMBER_OF_ARTICLES))
+    with requests.Session() as session:
+        new_articles = get_and_parse_random_articles(session, ArticleTitlesGet(rnlimit=NUMBER_OF_ARTICLES))
     article_service.add_articles_bulk(
         session=db_session,
         articles=[
