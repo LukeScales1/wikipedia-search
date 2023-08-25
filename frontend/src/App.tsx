f013cec07e54a60269b9b83076590648e5796f68
LukeScales1 <luke@nory.ai>
2023-08-24 18:48:34 -0700

    Installing sass, converting app css to scss

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 import { Articles } from "./features/articles/Articles";
 import { Search } from "./features/search/Search";
 import { useGetArticlesQuery, useGetSearchResultsQuery } from "./redux/apiSlice";
